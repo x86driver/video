8698f73d3495b024c08c51b4a0d5d37e7191ff0e
Lin Doremi <Doremi.Lin@garmin.com>
2010-07-21 09:22:13 +0800

    正確處理 yuv -> rgb 的轉換

--- yuv.c
+++ yuv.c
@@ -28,9 +28,9 @@ int main()
 		if (r > 255) r = 255;
 		if (g > 255) g = 255;
 		if (b > 255) b = 255;
-//		if (r < 0) r = 0;
-//		if (g < 0) g = 0;
-//		if (b < 0) b = 0;
+		if (r < 0) r = 0;
+		if (g < 0) g = 0;
+		if (b < 0) b = 0;
 		rgb.r = r;
 		rgb.g = g;
 		rgb.b = b;
@@ -42,9 +42,9 @@ int main()
                 if (r > 255) r = 255;
                 if (g > 255) g = 255;
                 if (b > 255) b = 255;
-//                if (r < 0) r = 0;
-//                if (g < 0) g = 0;
-//                if (b < 0) b = 0;
+                if (r < 0) r = 0;
+                if (g < 0) g = 0;
+                if (b < 0) b = 0;
 		rgb.r = r;
 		rgb.g = g;
 		rgb.b = b;
