9429d31f6d9bbbc2d4db7dbb048dc8f30df3945b
Lin Doremi <Doremi.Lin@garmin.com>
2010-08-02 12:12:42 +0800

    分析 parse 分析出來的資料 預期目標是會分析每個 control byte 出現的次數

---
+++ parse2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 256
+unsigned char *data;
+unsigned char state_table[] = {0x9d, 0xda, 0xf1, 0xb6};
+unsigned char lookup_table[] = {0x80, 0xc7, 0xec, 0xab};
+int count = 0;
+
+void lookup(int state, int *n)
+{
+	int i = *n;
+	int number = 0;
+	while (i < count) {
+		if (data[i+1] != lookup_table[state]) {
+			printf("It is 0x%X, next must be 0x%X, but next is: 0x%X\n", data[i], lookup_table[state], data[i+1]);
+			++i;
+		} else {
+//			printf("0x%X 268 0x%X 1440\n", data[i], data[i+1]);
+			i += 2;
+			++number;
+		}
+		if (data[i] != state_table[state]) {
+			printf("0x%X 268 0x%X 1440\n", state_table[state], lookup_table[state]);
+			printf("====== %d times ======\n", number);
+			break;
+		}
+	}
+	*n = i;
+}
+
+void parse()
+{
+	int i = 0;
+	while (i <= count) {
+		switch (data[i]) {
+		case 0x9d:
+			lookup(0, &i);
+			break;
+		case 0xda:
+			lookup(1, &i);
+			break;
+		case 0xf1:
+			lookup(2, &i);
+			break;
+		case 0xb6:
+			lookup(3, &i);
+			break;
+		default:
+			++i;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	FILE *fp = fopen("b.txt", "r");
+	if (!fp) {
+		perror("fopen");
+		exit(1);
+	}
+
+	data = (unsigned char*)malloc(SIZE);
+
+	fscanf(fp, "%x", (data+count));
+	while (!feof(fp)) {
+		++count;
+		fscanf(fp, "%x", (data+count));
+	}
+
+	--count;
+	parse();
+
+	free(data);
+	fclose(fp);
+	return 0;
+}
