01301893ac8011aeafabad9c7aabef13b8a9d308
Lin Doremi <Doremi.Lin@garmin.com>
2010-08-13 16:35:14 +0800

    採用新的方法轉換 YUV420
    目前這個方法似乎能正確把 FPGA 的資料轉出來

--- trans.c
+++ trans.c
@@ -7,6 +7,9 @@ struct RGB {
 
 int main()
 {
+	unsigned int x = 720;
+	unsigned int y = 240;
+
 	FILE *fp = fopen("my.raw", "rb");
 	FILE *out = fopen("out.raw", "wb");
 	if (!fp) {
@@ -18,16 +21,41 @@ int main()
 		exit(1);
 	}
 
-	struct RGB rgb;
-	unsigned char data;
-	unsigned int i = 0;
-
-	for (; i < 240*136; ++i) {
-		fread((void*)&data, sizeof(data), 1, fp);
-		rgb.r = rgb.g = rgb.b = data;
-		fwrite((void*)&rgb, sizeof(rgb), 1, out);
+	unsigned char *buf = malloc(x*y*2);
+	unsigned char outbuf[4];
+	if (buf == NULL) {
+		perror("malloc");
+		fclose(fp);
+		fclose(out);
+		exit(1);
 	}
 
+	fread((void*)buf, x*y*2, 1, fp);
+	unsigned char *ptrY = buf;
+	unsigned char *ptrU = buf+(x*y);
+	unsigned char *ptrV = buf+(x*y)+(x*y/2);
+
+	unsigned int i = 0;
+
+	for (; i < x*y/2; ++i) {
+		outbuf[0] = *ptrU++;
+		outbuf[1] = *ptrY++;
+		outbuf[2] = *ptrV++;
+		outbuf[3] = *ptrY++;
+		fwrite((void*)&outbuf[0], sizeof(outbuf), 1, out);
+	}
+
+
+/*
+	unsigned char rgb[3];
+	for (; i < 480*272; ++i) {
+		rgb[0] = *ptrY++;
+		rgb[1] = *ptrU++;
+		rgb[2] = *ptrY++;
+		fwrite((void*)&rgb[0], sizeof(rgb), 1, out);
+	}
+*/
+	free(buf);
 	fclose(fp);
 	fclose(out);
 	return 0;
